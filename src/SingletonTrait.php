6bbd455f06948e143ec902bfda4d5b5a13298f2b
flyinghail <flyinghail@msn.com>
2019-05-20 13:08:04 +0800

    init code

---
+++ src/SingletonTrait.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Hail\Singleton;
+
+trait SingletonTrait
+{
+    protected static $instance;
+
+    /**
+     * @return static
+     */
+    final public static function getInstance()
+    {
+        return static::$instance[static::class] ?? (static::$instance[static::class] = new static);
+    }
+
+    /**
+     * Singleton constructor.
+     */
+    final private function __construct()
+    {
+        $this->init();
+    }
+
+    /**
+     * Initializes the singleton
+     */
+    protected function init(): void
+    {
+    }
+
+    final public function __wakeup(): void
+    {
+    }
+
+    final private function __clone()
+    {
+    }
+}
